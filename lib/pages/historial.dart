2d60ab9b38395a09528976a2890f8efaa80b9083
santi0ne <johsrodr@espol.edu.ec>
2025-05-24 23:03:50 -0500

    feat: add navigation between pages

---
+++ lib/pages/historial.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class VetHistorialPage extends StatelessWidget {
+  const VetHistorialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Vista de Historial',
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: Colors.black87,
+        ),
+      ),
+    );
+  }
+}
